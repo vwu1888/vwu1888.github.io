0a78ae6f5f63ca414ad42f4b940d641fc16c2bdd
vwu <vwu1888@gmail.com>
2025-11-17 02:02:25 -0500

    changed base to repo name

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/portfolio/',
   plugins: [vue(), vueDevTools()],
   server: {
     host: true,
